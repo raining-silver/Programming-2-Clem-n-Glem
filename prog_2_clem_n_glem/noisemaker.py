c0e461bbf453012f0287e7866756ffa5f3344f58
Glem_Tucker <glemt262@hsd.k12.or.us>
2023-02-02 14:41:36 -0800

    yes

--- prog_2_clem_n_glem/noisemaker.py
+++ prog_2_clem_n_glem/noisemaker.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QLabel
+from PyQt6.QtWidgets import (QApplication, QVBoxLayout, QWidget, 
+                             QLabel, QHBoxLayout, QLineEdit, 
+                             QPushButton, QTextEdit)
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import Qt
 import sys
@@ -14,10 +16,27 @@ class Window(QWidget):
     def UI(self):
         layout = QVBoxLayout()
         self.setLayout(layout)
- 
-        label = QLabel("Hello World")
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(label)
+
+        #Create our widgets
+        title_label = QLabel("Goober")
+        title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        description = "Use the Noise Background generator to make"
+        description = " cool new backgrounds."
+        description_label = QLabel(description)
+
+        search_layout = QHBoxLayout()
+        self.search_field = QLineEdit()
+        search_button = QPushButton("   ")
+        search_layout.addWidget(self.search_field)
+        search_layout.addWidget(search_button)
+        
+        results_text = QTextEdit("      ")
+        
+        layout.addWidget(title_label)
+        layout.addWidget(description_label)
+        layout.addLayout(search_layout)
+        layout.addWidget(results_text)
 
 def main(): 
     app = QApplication(sys.argv)
